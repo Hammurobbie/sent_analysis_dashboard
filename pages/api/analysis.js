f791937d13f31959a733eb1ca0ace0e4a8301603
hammurobbie <rthomporter@gmail.com>
2022-11-30 23:21:59 -0600

    clean up

--- pages/api/analysis.js
+++ pages/api/analysis.js
@@ -2,7 +2,6 @@
 import { PythonShell } from "python-shell";
 
 export default function handler(req, res) {
-  let pyRes;
   let options = {
     mode: "text",
     pythonPath: "/usr/local/bin/python3",
@@ -12,9 +11,8 @@ export default function handler(req, res) {
   };
 
   PythonShell.run("analysis.py", options, function (err, results) {
-    if (err) throw err;
+    if (err) res.status(500).json(err);
 
-    console.log("results: %j", results);
     res.status(200).json({
       scores: { positive: 0.8752, neutral: 0.0879, negative: 0.0369 },
       key_words: [
@@ -25,15 +23,4 @@ export default function handler(req, res) {
       test: results,
     });
   });
-
-  // setTimeout(() => {
-  // res.status(200).json({
-  //   scores: { positive: 0.8752, neutral: 0.0879, negative: 0.0369 },
-  //   key_words: [
-  //     { sentiment: "positive", word: "hospitable" },
-  //     { sentiment: "negative", word: "dirty" },
-  //     { sentiment: "neutral", word: "hotel" },
-  //   ],
-  // });
-  // }, 6000);
 }
